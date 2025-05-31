4aa4d9f59b7e8933c9f12c0afd32c881b032f875
Carlos Garcia <cratosmanolo88@gmail.com>
2025-05-31 15:30:21 -0600

    properties, CORS

--- src/main/java/com/example/com/example/crudReact/CrudSpringbootApplication.java
+++ src/main/java/com/example/com/example/crudReact/CrudSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudSpringbootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrudSpringbootApplication.class, args);
 	}
 }
